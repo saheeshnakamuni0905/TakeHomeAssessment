93ef2e5ce7f741c3decf4853fa3bb728f6f8d6a7
saheeshnakamuni0905 <kamunisaheeshna5@gmail.com>
2024-07-25 13:45:52 -0400

    Added views for Models

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Company, Location
+from .serializers import CompanySerializer, LocationSerializer
 
 # Create your views here.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
+
+class LocationViewSet(viewsets.ModelViewSet):
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
